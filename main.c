103acde79baf7a87be887730984d179cfd13b2bd
Laarabi Suleyman <suleyman.laarabi@epitech.eu>
2024-10-31 16:58:15 +0100

    fix

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** epichecker
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 4563;
+    int result = 0;
+    int index = 1;
+
+    while (n != 0) {
+        int c = n % 10;
+        result += c *;
+        n = n / 10;
+    }
+    
+}
